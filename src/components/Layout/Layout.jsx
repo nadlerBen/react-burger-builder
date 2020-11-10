8b5e07e1c0ad359c46961373977429fd4d1a6563
bennadler <bennadlerr@gmail.com>
2020-11-10 19:01:44 +0200

    added layout css :snowflake:

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
   <Aux>
-    <div>
-      Toolbar, SideDrawer, Backdrop
-  </div>
-    <main>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
